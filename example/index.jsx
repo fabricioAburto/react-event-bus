3cd6768de7405066700623446e3b1002b3336d3b
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-11-06 21:11:25 +0100

    examples project

---
+++ example/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import 'react-app-polyfill/ie11';
+import ReactDOM from 'react-dom/client';
+
+import EventBusProvider, { useEventBus, useBusEffectOn } from '../dist';
+
+const MyLabel = () => {
+  const [state, setState] = useState('');
+  useBusEffectOn('print', val => setState(val));
+  return <h1>{state}</h1>;
+};
+
+const MyLabel3 = () => {
+  return <h1>{Date.now()}</h1>;
+};
+
+const App = () => {
+  const { dispatch } = useEventBus();
+  return (
+    <div title="Pepex">
+      <button onClick={() => dispatch('print', Date.now())}>Click</button>
+      <MyLabel />
+      <MyLabel3 />
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <EventBusProvider debug logPrefix="test">
+    <App />
+  </EventBusProvider>,
+);
